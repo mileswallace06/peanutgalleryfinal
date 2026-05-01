d724fadb2b54a86a2f331f5e6d64b0bb1934317d
Miles Wallace <mileswallace06@gmail.com>
2026-05-01 21:11:44 +0000

    File changes

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -27,8 +27,11 @@ export default function Layout() {
 
   return (
     <div className="min-h-screen rave-bg font-sans">
+      {/* Black banner at top */}
+      <div className="fixed top-0 left-0 right-0 h-24 bg-black z-[99]" />
+      
       {/* Logo */}
-      <Link to="/" className="fixed -top-16 left-0 right-0 z-[100] flex items-center justify-center pointer-events-auto">
+      <Link to="/" className="fixed top-0 left-0 right-0 z-[100] flex items-center justify-center pointer-events-auto h-40">
         <img
           src="https://media.base44.com/images/public/69ef9900cf3862dc0ea39734/f15cb860e_ChatGPTImageMay1202601_38_44PM.png"
           alt="Peanut Gallery"
