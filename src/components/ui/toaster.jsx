f2f75f7bd6f4bffc2c9ecbf7a5bcd8c7bce14636
Miles Wallace <mileswallace06@gmail.com>
2026-05-01 20:46:01 +0000

    Visual edits

--- src/components/ui/toaster.jsx
+++ src/components/ui/toaster.jsx
@@ -27,7 +27,7 @@ export function Toaster() {
           </Toast>);
 
       })}
-      <ToastViewport className="my-4 px-3 py-4 fixed top-0 z-[100] flex max-h-screen w-full flex-col-reverse sm:bottom-0 sm:right-0 sm:top-auto sm:flex-col md:max-w-[420px]" />
+      <ToastViewport className="my-4 px-1 py-4 fixed top-0 z-[100] flex max-h-screen w-full flex-col-reverse sm:bottom-0 sm:right-0 sm:top-auto sm:flex-col md:max-w-[420px]" />
     </ToastProvider>);
 
 }
