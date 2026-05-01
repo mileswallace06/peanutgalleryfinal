8574ceefae1a25f9561ac667759fb531c9575983
Miles Wallace <mileswallace06@gmail.com>
2026-05-01 20:55:36 +0000

    File changes

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -35,7 +35,7 @@ export default function Layout() {
               src="https://media.base44.com/images/public/69ef9900cf3862dc0ea39734/f15cb860e_ChatGPTImageMay1202601_38_44PM.png"
               alt="Peanut Gallery"
               className="w-full h-full object-contain object-center"
-              style={{ mixBlendMode: 'screen', transform: 'scale(3.5)', transformOrigin: 'center center' }}
+              style={{ mixBlendMode: 'screen', transform: 'scale(3.5) translateY(6px)', transformOrigin: 'center center' }}
             />
           </Link>
           {!user && (
